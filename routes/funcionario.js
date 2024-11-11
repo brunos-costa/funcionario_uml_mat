39fff422ed4f3f5ecc005b0d92cc91c88c5e00f8
Bruno Costa <analistabrunocosta@gmail.com>
2024-11-11 11:52:15 -0300

    Implementado exibiçao dos dados e exclusão

--- routes/funcionario.js
+++ routes/funcionario.js
@@ -24,15 +24,17 @@ router.post('/store',async(req, res)=>{
 })
 
 //2ª ROTA - EXIBIR PÁGINA RAIZ DE FUNCIONÁRIO
-router.get('/',(req, res)=>{
-    res.send("<h1> Página inicial de Funcionário</h1>")
+router.get('/show',(req, res)=>{
+   res.render('funcionario/index')
+
 })
 
 //3ª ROTA - CONSULTAR DADOS DO BANCO
-router.get('/show',async(req, res)=>{
-    let resultado = await funcionario.findAll()
+router.get('/',async(req, res)=>{
+    let resultado = await funcionario.findAll({include:departamento})// o include:departamento é como o sequelize faz para poder realizar consultas com join
     if(resultado){
         console.log(resultado)
+        res.render('funcionario/index',{dados:resultado})
     }
     else{
         console.log("Não foi possível exibir os dados")
@@ -40,18 +42,28 @@ router.get('/show',async(req, res)=>{
 })
 
 //4ª ROTA - DELETAR DADOS DO TABELA
-// :id significa que iremos passar uma valor na rota, ou seja, iremos informar um valor que poderá ser diferente e que será armazenado pela variável :id
+// :id significa que iremos passar um valor na rota, ou seja, iremos informar um valor que poderá ser diferente e que será armazenado pela variável :id
 router.get('/destroy/:id',async(req, res)=>{
     const resultado = await funcionario.destroy({
         where:{
             id:req.params.id// estamos recebendo o id via parâmetro que está sendo passado na rota, no caso, é o :id que estamos recebendo.
         }
     })
+    res.redirect('/')
 })
 
 //5ª ROTA - EXIBIR FORMULÁRIO DE CADASTRO
-router.get('/create',(req, res)=>{
-    res.render('funcionario/addFuncionario')
+router.get('/create',async(req, res)=>{
+    let resultado = await departamento.findAll()
+    if(resultado){
+        console.log(resultado)
+        res.render('funcionario/addFuncionario',{dados:resultado})
+    }
+    else{
+        console.log("Não foi possível carregar nenhum dado")
+        res.redirect('/')// redirecionando para a página inicial
+    }
+    
 })
 
 module.exports = router
